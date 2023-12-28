8089c007bc85b54ccce2ffb9225fd7e7e44b4075
slynk-0 <brenobertunes05@gmail.com>
2023-12-27 22:27:20 -0300

    Primeiro Commit para a atualização no repertório. Nenhuma modificação feita.

---
+++ App_Lista/app/src/main/java/devandroid/sodre/app_lista/MainActivity.java
@@ -0,0 +1,14 @@
+package devandroid.sodre.app_lista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
